7e0af3a1b20cc207d2f231d15508e7fb45378b00
su-hss <htetsusan.mdy@gmail.com>
2024-09-12 19:39:18 +0100

    add app.js file

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
-const ws = require ('ws')
-const server = new ws.Server({port: '3000'})
+const ws = require('ws')
+const server = new ws.Server({ port: '3000' })
 
-server.on('connection', socket =>{
-    socket.on('message', message =>{
-        console.log(message)
+server.on('connection', socket => {
+    socket.on('message', message => {
+        const b = Buffer.from(message)
+        console.log(b.toString())
+      //  console.log(message)
         socket.send(`${message}`)
     })
 })
