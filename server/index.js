8dec532c26c2d491de47c2566e87aa903af3220a
su-hss <htetsusan.mdy@gmail.com>
2024-10-16 00:17:56 +0100

    implemented chat activity detection

--- server/index.js
+++ server/index.js
@@ -19,16 +19,35 @@ const expressServer = app.listen(PORT, ()=> {
 const io = new Server(expressServer,{
 
   cors: {
-    orgin: process.env.NODE_ENV == "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
+    orgin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
   }
 })
 
 io.on('connection', socket => {
   console.log(`User ${socket.id} connected`)
 
-    socket.on('message', data => {
-        console.log(data)
-        io.emit('message', `${socket.id.substring(0,5)}: ${data}`)
-    })
+
+  //Upon connection - only to user
+  socket.emit('message', "Welcome to Chat App!")
+
+  //Upon connection - to all others
+  socket.broadcast.emit('message', `User ${socket.id.substring(0,5)}} connected`)
+
+  //Listening for a message event
+  socket.on('message', data => {
+      console.log(data)
+      io.emit('message', `${socket.id.substring(0,5)}: ${data}`)
+  })
+
+  //when user disconnets - to all others
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('message',`User ${socket.id.substring(0,5)}} disconnected`)
+  })
+
+
+  // Listen for activity 
+  socket.on('activity', (name) => {
+    socket.broadcast.emit('activity', name)
+  })
 })
 
